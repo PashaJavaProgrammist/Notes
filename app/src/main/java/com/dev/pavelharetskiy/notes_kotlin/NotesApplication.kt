dd41006fe8f4b7a99bb59bfc18e4cba8f084c398
Pavel Haretskiy <pasha280294@gmail.com>
2018-04-09 02:07:12 +0300

    Fabric added

--- app/src/main/java/com/dev/pavelharetskiy/notes_kotlin/NotesApplication.kt
+++ app/src/main/java/com/dev/pavelharetskiy/notes_kotlin/NotesApplication.kt
@@ -1,14 +1,17 @@
 package com.dev.pavelharetskiy.notes_kotlin
 
 import android.app.Application
+import com.crashlytics.android.answers.Answers
 import com.raizlabs.android.dbflow.config.FlowConfig
 import com.raizlabs.android.dbflow.config.FlowManager
+import io.fabric.sdk.android.Fabric
 
 
 class NotesApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        Fabric.with(this, Answers())
         FlowManager.init(FlowConfig.Builder(this).build())
     }
 
