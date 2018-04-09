6082a46eb6fda5d2b71612aa52bf1262b04e1350
Pavel <pasha280292@gmail.com>
2018-04-09 15:54:25 +0300

    c

--- app/src/main/java/com/dev/pavelharetskiy/notes_kotlin/NotesApplication.kt
+++ app/src/main/java/com/dev/pavelharetskiy/notes_kotlin/NotesApplication.kt
@@ -3,6 +3,7 @@ package com.dev.pavelharetskiy.notes_kotlin
 import android.app.Application
 import com.crashlytics.android.Crashlytics
 import com.crashlytics.android.answers.Answers
+import com.crashlytics.android.core.CrashlyticsCore
 import com.raizlabs.android.dbflow.config.FlowConfig
 import com.raizlabs.android.dbflow.config.FlowManager
 import io.fabric.sdk.android.Fabric
@@ -14,13 +15,11 @@ class NotesApplication : Application() {
         Fabric.with(this, Answers())
 
         // Set up Crashlytics, disabled for debug builds
-//        val crashLyticsKit = Crashlytics.Builder()
-//                .core(CrashlyticsCore.Builder().disabled(BuildConfig.DEBUG).build())
-//                .build()
+        val crashLyticsKit = Crashlytics.Builder()
+                .core(CrashlyticsCore.Builder().disabled(BuildConfig.DEBUG).build())
+                .build()
 
-//        Fabric.with(this, crashLyticsKit)
-
-        Fabric.with(this, Crashlytics())
+        Fabric.with(this, crashLyticsKit)
 
         FlowManager.init(FlowConfig.Builder(this).build())
     }
