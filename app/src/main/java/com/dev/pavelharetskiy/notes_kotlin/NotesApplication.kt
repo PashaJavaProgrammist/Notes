b12092c06d6b019d6777dce3ca9f5553adb96970
Pavel Haretskiy <pasha280294@gmail.com>
2018-04-09 12:17:20 +0300

    crashlytics changed

--- app/src/main/java/com/dev/pavelharetskiy/notes_kotlin/NotesApplication.kt
+++ app/src/main/java/com/dev/pavelharetskiy/notes_kotlin/NotesApplication.kt
@@ -3,17 +3,23 @@ package com.dev.pavelharetskiy.notes_kotlin
 import android.app.Application
 import com.crashlytics.android.Crashlytics
 import com.crashlytics.android.answers.Answers
+import com.crashlytics.android.core.CrashlyticsCore
 import com.raizlabs.android.dbflow.config.FlowConfig
 import com.raizlabs.android.dbflow.config.FlowManager
 import io.fabric.sdk.android.Fabric
-
 
 class NotesApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
         Fabric.with(this, Answers())
-        Fabric.with(this, Crashlytics())
+
+        // Set up Crashlytics, disabled for debug builds
+        val crashLyticsKit = Crashlytics.Builder()
+                .core(CrashlyticsCore.Builder().disabled(BuildConfig.DEBUG).build())
+                .build()
+
+        Fabric.with(this, crashLyticsKit)
 
         FlowManager.init(FlowConfig.Builder(this).build())
     }
